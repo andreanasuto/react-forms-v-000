8bfe7c8d19a95ce2a16c450367583269ea5c3ed9
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-23 22:25:27 +0000

    automatically backed up by learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -14,9 +14,14 @@ export default class ControlledInput extends React.Component {
       value: event.target.value,
       })
   }
+
   render() {
     return (
-      
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+      />
     )
   }
 }
